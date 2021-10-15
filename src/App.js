ba32ed86c7c1106966a42a56d054f37e29ef11cc
JasonRillera <jarillera@gmail.com>
2021-10-15 09:03:53 -0700

    update homepage

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-            
                 <div className="footer">
                     <Typography.Title level={5} style={{ color: "white", textAlign: "center" }}>
                         CryptoVerse <br />
