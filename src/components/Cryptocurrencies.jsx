fadb0dda46fe21aaa068f27311f5331cd2d50876
JasonRillera <jarillera@gmail.com>
2021-10-15 08:57:23 -0700

    update layout

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cryptocurrencies = () => {
+    return (
+        <div>
+            I and the crypto money page
+        </div>
+    )
+}
+
+export default Cryptocurrencies
