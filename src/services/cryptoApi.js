20ee7992bb418a262f91319bb9e06d88676635e8
JasonRillera <jarillera@gmail.com>
2021-10-16 12:32:28 -0700

    update homepage

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,16 +5,16 @@ const cryptoApiHeaders = {
     'x-rapidapi-key': '4306310f3fmshc73444866c39fd4p1b3086jsn3a95a1520d40'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/stats'
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
-const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/coins')
+            query: (count) => createRequest(`/coins?limit=${count}`),
         })
     })
 });
